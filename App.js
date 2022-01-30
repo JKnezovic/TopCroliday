42af0d8979622c5efe5a718982bb3b2e2a5e2b30
JKnezovic <jo.knezovic@gamil.com>
2022-01-30 20:36:18 +0100

    added parse keys and file to fix errors

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Parse from "parse/react-native.js";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+Parse.setAsyncStorage(AsyncStorage);
+//You need to copy BOTH the the Application ID and the Javascript Key from: Dashboard->App Settings->Security & Keys 
+Parse.initialize('F4H5ofBLuLAIxCkwAE2E2oS2Hn1b5kppANz13hd0','tZrBhwOCMFkTbg1WJcWzgjVgoJ38sK5Xb0QjpywB');
+Parse.serverURL = 'https://parseapi.back4app.com/';
 
 export default function App() {
   return (
