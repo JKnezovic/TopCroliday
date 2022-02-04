482cc4dbf4eed7827d20c1b0975f42f45ad727ed
JKnezovic <jo.knezovic@gamil.com>
2022-02-04 16:03:30 +0100

    Main screen style added

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -1,20 +1,30 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MainScreenTile from './MainScreenTiles';
-
-const images = {
-  pre: require('../assets/pre.jpg'),
-  post: require('../assets/post.jpg'),
-  during: require('../assets/during.jpeg')
+const data = {
+  pre: {
+    uri:require('../assets/pre.jpg'),
+    title: 'Pre Stay',
+    desc: 'Here you can explore and pick all the top activities you would like to try and other services you may need.'
+  },
+  post: {
+    uri: require('../assets/post.jpg'),
+    title: 'During Stay',
+    desc: 'Take a look at some sights worth exploiting, restaurants, beaches, museums and galleries worth visiting.'
+  },
+  during: {
+    uri: require('../assets/during.jpeg'),
+    title: 'Post Stay',
+    desc: 'Tell us how did you like your accommodation as well as was this app any helpful to you?'
+  }
 }
 
 const MainScreen = () => {
   return (
       <View style={styles.container}>
-      <MainScreenTile imageUri={images.pre} title={"Pre Stay"}></MainScreenTile>
-      <MainScreenTile imageUri={images.during} title={"During Stay"}></MainScreenTile>
-      <MainScreenTile imageUri={images.post} title={"Post Stay"}></MainScreenTile>
-  
+        <MainScreenTile imageUri={data.pre.uri} title={data.pre.title} desc={data.pre.desc}></MainScreenTile>
+        <MainScreenTile imageUri={data.during.uri} title={data.during.title} desc={data.during.desc}></MainScreenTile>
+        <MainScreenTile imageUri={data.post.uri} title={data.post.title} desc={data.post.desc}></MainScreenTile>
       </View>
   );
 };
--- components/MainScreenTiles.js
+++ components/MainScreenTiles.js
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-
+import { LinearGradient } from 'expo-linear-gradient';
+import { AntDesign } from '@expo/vector-icons';
 
 const MainScreenTile = (props) => {
   return (
       <View style={styles.container}>
           <ImageBackground source={props.imageUri} resizeMode="cover" style={styles.image}>
-            <Text>{props.title}</Text>
+              <LinearGradient 
+              style={styles.textBox}
+              colors={['transparent','rgba(0,0,0,0.6)']}>
+                <Text style={styles.textBig}>
+                  {props.title} 
+                  <AntDesign name="right" size={30} color="white" />
+                </Text>
+                <Text style={styles.desc}>{props.desc}</Text>
+              </LinearGradient>
           </ImageBackground>
       </View>
   );
@@ -19,14 +28,31 @@ const styles = StyleSheet.create({
     flex: 1,
     width:'95%',
     paddingVertical: 6,
-    // borderRadius: 50,
     marginBottom: 6,
   },
   image: {
     flex: 1,
     borderRadius:15,
     overflow:'hidden'
-
-   
   },
+  textBox:{
+    position:'absolute',
+    right:0,
+    bottom:0,
+    width:'100%'
+
+  },
+  textBig:{
+    fontSize: 42,    
+    color:'white',
+    textAlign:'right',
+    paddingRight:'2%'
+  },
+  desc:{
+    fontSize:12,    
+    color:'white',
+    paddingLeft:'2%',
+    paddingBottom:'2%'
+  }
+
 });
