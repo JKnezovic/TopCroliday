a2a64538fb04bfdce34d4e1096c28ab411e59773
JKnezovic <jo.knezovic@gamil.com>
2022-03-27 20:27:35 +0200

    implemented post stay

--- components/PostStay/PostStay.js
+++ components/PostStay/PostStay.js
@@ -1,11 +1,30 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Linking } from 'react-native';
+import PostStayTile from './PostStayTile';
 
+
+
+const Accommodation = () => <Text style={{marginRight:'10%',flexShrink:1,alignSelf:'center'}}> 
+If you liked your accommodation and would like to give some feedback feel free to leave a review   
+<Text style={{color:'blue'}} onPress={()=>Linking.openURL('https://g.page/r/CfDYgy9-pzo7EAg/review')}> here</Text>
+</Text>;
+
+const TopCroliday = () => <Text style={{marginRight:'10%',flexShrink:1,alignSelf:'center'}}> 
+If you liked your experience with TopCroliday and would like to give some feedback feel free to leave a review   
+<Text style={{color:'blue'}} onPress={()=>Linking.openURL('https://g.page/r/CfDYgy9-pzo7EAg/review')}> here</Text>
+</Text>;
+
+const App = () => <Text style={{marginRight:'10%',flexShrink:1,alignSelf:'center'}}> 
+Help us improve this app and your experience using it by leaving a comment:
+</Text>;
 
 const PostStay = (props) => {
   return (
       <View style={styles.container}>
-          <Text>PostStay</Text>
+          <PostStayTile number={1}><Accommodation/></PostStayTile>
+          <PostStayTile number={2}><TopCroliday/></PostStayTile>
+          <PostStayTile number={3}><App/></PostStayTile>
+          
       </View>
   );
 };
@@ -16,6 +35,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems:'center',
-    justifyContent:'center'
   }
 })
